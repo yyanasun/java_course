d7f52b6dcee583c70a210bfc18feaaf9b9e9eee4
yyanasun <yyanasun@gmail.com>
2016-12-25 10:59:00 +0200

    Зміна структури програмного коду

--- sandbox/src/main/java/ru/stqa/javacourse/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/javacourse/sandbox/MyFirstProgram.java
@@ -2,5 +2,7 @@ package ru.stqa.javacourse.sandbox;
 
 public class MyFirstProgram {
 
-public static void main(String[] args) { System.out.println("Hello,world!");}
+  public static void main(String[] args) {
+    System.out.println("Hello,world!");
+  }
 }
